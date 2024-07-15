ce26ba730e4cc8eae9473ae7a190c1431e05df29
ShreyanshVennamaneni <144498974+anshu7600@users.noreply.github.com>
2024-07-15 18:38:02 -0500

    commit initial

---
+++ Assets/Stock Sim.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+
+
+public class StockSim : MonoBehaviour
+{
+    // I am wasting my time by typing this
+    public float CurrentPrice = 1;
+    // Delta means change just is case you forgot already :/
+    public float DeltaPrice;
+    // Trends are ordered by influence: Company Trend(CT), Stock Trend(ST), Tick Trend(TT)
+    public float[] Trends = {1, 1, 1};
+    // Trend Timer is only used by ST
+    public float TrendTimer;
+    // Probability is used to decide Tick Trend and Stock Trend as they are influenced by another trend
+    // 0 to 9 used by ST when CT is Bull | 10 to 19 used by ST when CT is Bear
+    // 3 to 6 is used by TT when ST is Bull | 13 to 16 is used by TT when ST is Bear | 19 and 20 are used by TT when ST is Cons(olidating)
+    private readonly int[] Probablity = {0, -1, -1, -1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, -1, -1, -1, -1, 0, -1, 1};
+    // TPM stands for ticks per minute, Each tick the stock updates
+    public int TPM = 10;
+    private float TickTimer = 0;
+    // Stock Factors include: Peak(Max value of stock), Valley(Min value of stock), Risk(Multiplies losses),(Ignore the 9.11), Reward(Multiplies Gains)
+    public float[] StockFactors = {100f, 2f, 0.97f, 9.11f, 1.12f};
+
+    // Functions
+    // Logger: Outputs all the values in the console
+    private void Logger() 
+    {
+        // Debug Purposes only, Change values as you like
+        Debug.Log("Current Price: " + CurrentPrice + "$ Delta Price: " + DeltaPrice + "$ CTrend: " + Trends[0] + " STrend: " + Trends[1] + " Timer: " + TrendTimer + " TTrend: " + Trends[2]);
+        //Debug.Log(StockFactors);
+    }
+
+    // TrendManager: Does all the trend related stuff(Further explanations are shown below)
+    private void TrendManager()
+    {
+        // This function is split into 3 sections
+        // Company Trend: Checks if Peak or Valley is hit and sets accordingly (It goes dow after a peak and up after a valley)
+        if (CurrentPrice >= StockFactors[0]) 
+        {
+            Trends[0] = -1;
+        }
+        else if (CurrentPrice <= StockFactors[1])
+        {
+            Trends[0] = 1;
+        }
+
+        // Stock Trend: Checks if the Trend timer is over and then updates the trend randomly (Influnced by CT)
+        if (math.floor(TrendTimer/10) == (TrendTimer - (10*math.floor(TrendTimer / 10))))
+        {
+            if (Trends[0] == 1)
+            {
+                Trends[1] = Probablity[UnityEngine.Random.Range(0, 10)];
+            }
+            else if (Trends[0] == -1)
+            {
+                Trends[1] = Probablity[UnityEngine.Random.Range(10, 20)];
+            }
+            TrendTimer = (10 * UnityEngine.Random.Range(3, 9));
+        }
+
+        // Tick Trend: Is random, and decides if the stock price will go up or down (Influenced by ST)
+        if (Trends[1] == 1)
+        {
+            Trends[2] = Probablity[UnityEngine.Random.Range(3, 7)];
+        }
+        else if (Trends[1] == -1)
+        {
+            Trends[2] = Probablity[UnityEngine.Random.Range(13, 7)];
+        }
+        else 
+        {
+            Trends[2] = Probablity[UnityEngine.Random.Range(19, 21)];
+        }
+
+        // Updates the timer come on its obivous
+        TrendTimer += 1;
+
+    }
+
+    // TickCounter: Makes all the tick the same distance apart(Time wise)
+    private void TickCounter()
+    {
+        TickTimer += Time.deltaTime;
+        if (TickTimer > 60/TPM - Time.deltaTime) 
+        {
+            TickTimer = 0;
+            TickUpdate();
+        }
+    }
+
+    // TickUpdate: Everything that happens in a tick is here
+    private void TickUpdate()
+    {
+        // Sets DeltaPrice to a value influenced by the TT and ST
+        if (Trends[1] == Trends[2]) 
+        {
+            DeltaPrice = UnityEngine.Random.Range(1, 11);
+        }   
+        else 
+        {
+            DeltaPrice = UnityEngine.Random.Range(1, 21);
+        }
+
+        // Multiplies DeltaPrice by the TT and then by Risk/Reward
+        DeltaPrice *= Trends[2];
+        DeltaPrice *= StockFactors[(int)Trends[2] + 3];
+
+        // Adds the Price over to the CurrentPrice
+        CurrentPrice += DeltaPrice;
+        if (CurrentPrice < 0)
+        {
+            CurrentPrice = 0;
+        }
+
+        // Debug Purposes only
+        Logger();
+        // Calls Trend Manager
+        TrendManager();
+        
+    }
+
+        // Start is called before the first frame update 
+        // We know unity, we know...
+        void Start()
+    {
+        TrendTimer = (10 * UnityEngine.Random.Range(3, 9));
+    }
+
+    // Update is called once per frame
+    // tHaTs CrAzY!!
+    void Update()
+    {
+        TickCounter();
+    }
+}
---
+++ Assets/Stock Test.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockTest : MonoBehaviour
+{
+    public float DeltaP;
+    public float CP = 20;
+    private float CurrentTrend;
+    private float trend;
+    public string Trend;
+    public float TrendTimer = 0;
+    public float TrendTime;
+    private float TickTimer;
+    public int tpm = 10;
+    public float Risk = 0.97f;
+    public float Reward = 1.24f;
+    private readonly int[] bull = {1, 1, 1, -1};
+    private readonly int[] bear = { -1, -1, -1, 1 };
+    public float Peak;
+    public float Valley;
+    public float CompanyTrend;
+
+
+    private void UpdateTrend()
+    {
+        trend = Random.Range(-1, 2);
+        if (trend == 0)
+        {
+            Trend = "Cons";
+        }
+        else if (trend == -1) 
+        {
+            Trend = "Bull";
+        }
+        else if (trend == 1) 
+        {
+            Trend = "Bear";
+        }
+
+    }
+
+    private void TrendCounter() 
+    {
+        if (TrendTimer == TrendTime) 
+        {
+            TrendTime = Random.Range(3, 9);
+            TrendTimer = 0;
+            UpdateTrend();
+        }
+    }
+
+    private void TickUpdate()
+    {
+        TrendTimer += 1;
+
+        if (Trend == "Bull")
+        {
+            CurrentTrend = bull[Random.Range(0, 5)];
+        }
+        else if (Trend == "Bear")
+        {
+            CurrentTrend = bear[Random.Range(0, 5)];
+        }
+        else 
+        {
+            CurrentTrend = Random.Range(0, 2);
+            if (CurrentTrend == 0)
+            {
+                CurrentTrend = -1;
+            }
+        }
+
+        if (CurrentTrend == trend)
+        {
+            DeltaP = Random.Range(1, 21);
+        }
+        else 
+        {
+            DeltaP = Random.Range(1, 11);
+        }
+
+        DeltaP = DeltaP * CurrentTrend;
+
+        if (DeltaP < 0)
+        {
+            DeltaP = DeltaP * Risk;
+        }
+        else 
+        {
+            DeltaP = DeltaP * Reward;
+        }
+
+        CP += DeltaP;
+        if (CP < 0)
+        {
+            CP = 0;
+        }
+
+        TrendCounter();
+        Debug.Log("Cp: " + CP + "$ DeltaP: " + DeltaP + "$ Trend: " + Trend);
+    }
+
+
+    private void TickCounter() 
+    {
+        TickTimer += Time.deltaTime;
+        if (TickTimer > (60/tpm-Time.deltaTime))
+        { 
+            TickTimer = 0;
+            TickUpdate();
+        }
+    }
+    
+    void Start()
+    {
+        Debug.Log("Its a brand new day at Wall Street!");
+        TrendTime = Random.Range(3, 16);
+        TrendTimer = 0;
+        UpdateTrend();
+        Risk = 0.97f;
+        Reward = 1.24f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        TickCounter();
+    }
+}
